9189d4d5316afe7ac5bd1a034cfe1e124fbde8de
LL\snekumar1 <sneha.kumari@publicissapient.com>
2021-09-01 21:41:39 +0530

    carousel fix

--- components/Carousel/CarouselComponent.js
+++ components/Carousel/CarouselComponent.js
@@ -26,39 +26,43 @@ const CarouselComponent = ({data}) => {
   
     const slides = items.map((item, index) => {
       return (
-       
-            <CarouselItem
+      <CarouselItem
           onExiting={() => setAnimating(true)}
           onExited={() => setAnimating(false)}
           key={item?.url}
         >
-          <div className="carouselItem">
-
-
-<Image
-        media={item?.url} 
-        className="w-full"
-      />
-         
-          </div>
-        </CarouselItem>
-
-       
-        
+      <div className="carouselItem">
+        <Image
+          media={item?.url} 
+          className="w-full"
+        />
+        <div className="absolute inset-x-0 top-60">
+          <p className="text-8xl text-white font-bold text-center absolute inset-x-0 top-50 h-16">{item.alternativeText}</p>
+          <p className="text-3xl text-white font-semibold text-center absolute inset-x-0 top-40 h-16">{item.caption}</p>
+          {/* <a href={'/category/' + data.id} className="hover:no-underline">
+            <div className="flex flex-col justify-center items-center">
+              <button className="bg-white hover:bg-gray-200 text-4xl text-black font-bold py-2 px-2 rounded absolute top-60 ">
+                Explore {item.alternativeText}
+              </button>
+            </div>
+          </a> */}
+        </div>
+      </div>
+      </CarouselItem>
       );
     });
   
     return (
-          <Carousel
+        <Carousel
           activeIndex={activeIndex}
           next={next}
           previous={previous}
-          >
+        >
           <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
           {slides}
           <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
           <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-          </Carousel>
+        </Carousel>
         );
   }
   export default CarouselComponent;
